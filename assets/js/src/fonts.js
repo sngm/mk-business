5394126b0f41cbc3fabefa300a10f3f0a9b7ecac
sngm <magnus@goettingen.dev>
2025-08-30 15:54:09 +0200

    Font imports not used anymore, instead we use the Font Js Chunk Files

--- assets/js/src/fonts.js
+++ assets/js/src/fonts.js
@@ -11,7 +11,7 @@ const interFonts = import.meta.glob('../../fonts/inter-v19-latin/*.woff2');
 // Process all font imports
 Object.values(interTightFonts).forEach(importFn => importFn());
 Object.values(interFonts).forEach(importFn => importFn());
-import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-regular.woff2';
+/* import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-regular.woff2';
 import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-italic.woff2';
 import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-500.woff2';
 import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-500italic.woff2';
@@ -22,10 +22,10 @@ import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-700italic.woff2';
 import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-800.woff2';
 import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-800italic.woff2';
 import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-900.woff2';
-import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-900italic.woff2';
+import '../../fonts/inter-tight-v8-latin/inter-tight-v8-latin-900italic.woff2'; */
 
 // Inter fonts (all weights and styles)
-import '../../fonts/inter-v19-latin/inter-v19-latin-100.woff2';
+/* import '../../fonts/inter-v19-latin/inter-v19-latin-100.woff2';
 import '../../fonts/inter-v19-latin/inter-v19-latin-100italic.woff2';
 import '../../fonts/inter-v19-latin/inter-v19-latin-200.woff2';
 import '../../fonts/inter-v19-latin/inter-v19-latin-200italic.woff2';
@@ -42,4 +42,4 @@ import '../../fonts/inter-v19-latin/inter-v19-latin-700italic.woff2';
 import '../../fonts/inter-v19-latin/inter-v19-latin-800.woff2';
 import '../../fonts/inter-v19-latin/inter-v19-latin-800italic.woff2';
 import '../../fonts/inter-v19-latin/inter-v19-latin-900.woff2';
-import '../../fonts/inter-v19-latin/inter-v19-latin-900italic.woff2';
+import '../../fonts/inter-v19-latin/inter-v19-latin-900italic.woff2'; */
